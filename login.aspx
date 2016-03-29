6a47338718f94fac55f2e833514497446cfd2872
t1b1c <tiberiu.craciun@gmail.com>
2016-03-29 10:57:35 +0300

    recaptcha v2 and latest lwas

--- login.aspx
+++ login.aspx
@@ -1,12 +1,12 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="login" Theme="Default" %>
 <%@ Register Assembly="LWAS" Namespace="LWAS.Infrastructure" TagPrefix="LWASI" %>
-<%@ Register TagPrefix="recaptcha" Namespace="Recaptcha" Assembly="Recaptcha" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
     <form id="form1" runat="server">
@@ -14,22 +14,16 @@
 
     <div id="loginPanel">
         <div id="loginContainer">
-        <asp:Login ID="Login1" runat="server" onauthenticate="Login1_Authenticate" 
-            Height="200px" Width="400px" DisplayRememberMe="False" EnableTheming="True"  
-            TitleText="" LoginButtonText="Login" 
-            PasswordLabelText="Password" PasswordRequiredErrorMessage="Password is mandatory" 
-            UserNameLabelText="User" UserNameRequiredErrorMessage="User is mandatory"
-            FailureText="Your credentials are wrong. Try again!">
-        </asp:Login>
+            <asp:Login ID="Login1" runat="server" onauthenticate="Login1_Authenticate" 
+                Height="200px" Width="400px" DisplayRememberMe="False" EnableTheming="True"  
+                TitleText="" LoginButtonText="Login" 
+                PasswordLabelText="Parola" PasswordRequiredErrorMessage="Parola este obligatorie" 
+                UserNameLabelText="Utilizator" UserNameRequiredErrorMessage="Utilizatorul este obligatoriu"
+                FailureText="Ceva e gresit. Incearca din nou!">
+            </asp:Login>
         </div>
-        <div id="captchaContainer">
-        <recaptcha:RecaptchaControl
-            ID="recaptcha"
-            runat="server"
-            PublicKey=""
-            PrivateKey=""
-            Theme="white"
-            />
+        <div  id="captchaContainer" style="padding: 40px 20px;">
+            <div class="g-recaptcha" data-sitekey="6LcLPOwSAAAAABYdZOuzEsoliE0xJzQuzbHDL4L2"></div>
         </div>
     </div>
     </form>
--- login.aspx.cs
+++ login.aspx.cs
@@ -20,25 +20,75 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
 
 using LWAS.Infrastructure.Security;
 
 public partial class login : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void Page_Init(object sender, EventArgs e)
     {
+        //if (!Request.IsSecureConnection)
+          //Response.Redirect(Request.Url.ToString().Replace("http://", "https://"));
+    }
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+
         Settings settings = new Settings(this);
         this.Title = settings.TitleOnPage;
         Login1.TitleText = settings.TitleOnScreen;
-		
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
         Page.Validate();
         ((TextBox)Login1.FindControl("UserName")).Focus();
     }
     protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
     {
-        if (!Page.IsValid)
+        if (!Page.IsValid || !ValidateRecaptcha())
             e.Authenticated = false;
         else
             e.Authenticated = Authenticator.Instance.Verify(this.Login1.UserName, this.Login1.Password);
+    }
+
+    public bool ValidateRecaptcha()
+    {
+        string Response = Request["g-recaptcha-response"];//Getting Response String Append to Post Method
+        bool Valid = false;
+        //Request to Google Server
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create
+        (" https://www.google.com/recaptcha/api/siteverify?secret={secret-here}&response=" + Response);
+        try
+        {
+            //Google recaptcha Response
+            using (WebResponse wResponse = req.GetResponse())
+            {
+
+                using (StreamReader readStream = new StreamReader(wResponse.GetResponseStream()))
+                {
+                    string jsonResponse = readStream.ReadToEnd();
+
+                    JavaScriptSerializer js = new JavaScriptSerializer();
+                    RecaptchaVerificationResponse data = js.Deserialize<RecaptchaVerificationResponse>(jsonResponse);// Deserialize Json
+
+                    Valid = Convert.ToBoolean(data.success);
+                }
+            }
+
+            return Valid;
+        }
+        catch (WebException ex)
+        {
+            throw ex;
+        }
+    }
+
+    class RecaptchaVerificationResponse
+    {
+        public string success { get; set; }
     }
 }
